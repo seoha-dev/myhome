02e98e7207b02468fd9b602fd925a3e06fcc17da
seoha-dev <seoha.dev@gmail.com>
2022-06-27 19:38:26 +0900

    init

---
+++ src/test/java/com/study/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
